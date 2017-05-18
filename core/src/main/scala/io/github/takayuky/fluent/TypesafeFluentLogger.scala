cc3b5214c97019bb0182fd7c53f0cd62a53b8c65
Takayuki Sakai <kaky0922@gmail.com>
2017-05-18 17:16:19 +0900

    make TypesafeFluentLogger a value class

--- core/src/main/scala/io/github/takayuky/fluent/TypesafeFluentLogger.scala
+++ core/src/main/scala/io/github/takayuky/fluent/TypesafeFluentLogger.scala
@@ -2,7 +2,7 @@ package io.github.takayuky.fluent
 
 import org.fluentd.logger.scala.{FluentLogger, FluentLoggerFactory}
 
-final case class TypesafeFluentLogger(underlying: FluentLogger) {
+final case class TypesafeFluentLogger(underlying: FluentLogger) extends AnyVal {
   def log(label: String, data: Map[String, FluentAcceptable], timestamp: Long): Unit = {
     val validatedData = data.mapValues(_.v)
     underlying.log(label, validatedData, timestamp)
