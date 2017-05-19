64cd1673b50026d9f1b6447067ffc277470c364f
Takayuki Sakai <kaky0922@gmail.com>
2017-05-20 02:40:19 +0900

    add an example to use Lists

---
+++ examples/src/main/scala/io/github/takayuky/examples/ListExample.scala
@@ -0,0 +1,19 @@
+package io.github.takayuky.examples
+
+import io.github.takayuky.fluent._
+import io.github.takayuky.fluent.list._
+
+object ListExample {
+  def main(args: Array[String]): Unit = {
+    val tag = "basic_example"
+    val host = "localhost"
+    val port = 24224
+
+    val logger = TypesafeFluentLoggerFactory.getLogger(tag, host, port)
+
+    logger.log("success", Map(
+      "List" -> List(1,2,3),
+      "Option" -> Option(List("hello"))
+    ), 0L)
+  }
+}
