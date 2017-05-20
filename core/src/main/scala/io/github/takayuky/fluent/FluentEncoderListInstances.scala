f8f21206eb1e87cdf7dca5b407a66ab73ede42fa
Takayuki Sakai <kaky0922@gmail.com>
2017-05-20 12:25:02 +0900

    fix a bug in the encoder of lists

--- core/src/main/scala/io/github/takayuky/fluent/FluentEncoderListInstances.scala
+++ core/src/main/scala/io/github/takayuky/fluent/FluentEncoderListInstances.scala
@@ -3,6 +3,6 @@ package io.github.takayuky.fluent
 trait FluentEncoderListInstances {
   implicit def fluentEncoderForTraversableOnce[A](implicit A: FluentEncoder[A]): FluentEncoder[TraversableOnce[A]] =
     FluentEncoder.create { toa =>
-      ConcreteFluentAcceptable(toa.map(A.encode))
+      ConcreteFluentAcceptable(toa.map(A.encode(_).v))
     }
 }
